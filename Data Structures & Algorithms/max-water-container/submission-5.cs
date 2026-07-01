d29d43babfb893d467c4ae3fdee2766a1e9f1820
George <56749275+nfd74108520@users.noreply.github.com>
2026-07-01 17:08:41 +0800

    Add: max-water-container - submission-5

---
+++ Data Structures & Algorithms/max-water-container/submission-5.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int result = 0;
+
+        int l = 0, r = heights.Length - 1;
+
+        while (l < r) {
+            int w = r - l;
+            int h = heights[l] < heights[r] ? heights[l] : heights[r];
+            result = (w * h) > result ? w * h : result;
+
+            if (heights[l] < heights[r])
+                l++;
+            else
+                r--;
+        }
+
+        return result;
+    }
+}
