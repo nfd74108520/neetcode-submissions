5599370e8b20a43b73725b32a33182ccbceea5d3
George <56749275+nfd74108520@users.noreply.github.com>
2026-07-02 11:12:44 +0800

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Trap(int[] h) {
+        int l = 0, r = h.Length - 1;
+        int lMax = 0, rMax = 0;
+        int total = 0;
+
+        while (l < r) {
+            if (h[l] < h[r]) {
+                if (h[l] > lMax)
+                    lMax = h[l];
+                else
+                    total += lMax - h[l];
+                l++;
+            } else {
+                if (h[r] > rMax)
+                    rMax = h[r];
+                else
+                    total += rMax - h[r];
+                r--;
+            }
+        }
+
+        return total;
+    }
+}
