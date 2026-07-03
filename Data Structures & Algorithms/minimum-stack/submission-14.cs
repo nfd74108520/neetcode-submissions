d30b9cfb992b308a72399071d866e14f5b164d8d
George <56749275+nfd74108520@users.noreply.github.com>
2026-07-03 09:25:00 +0800

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.cs
@@ -0,0 +1,27 @@
+public class MinStack {
+    Stack<(int value, int minVal)> _stack;
+
+    public MinStack() {
+        _stack = new Stack<(int value, int minVal)>();
+    }
+
+    public void Push(int val) {
+        int newMin = val;
+        if (_stack.Count != 0)
+            newMin = Math.Min(_stack.Peek().minVal, val);
+
+        _stack.Push((val, newMin));
+    }
+
+    public void Pop() {
+        _stack.Pop();
+    }
+
+    public int Top() {
+        return _stack.Peek().value;
+    }
+
+    public int GetMin() {
+        return _stack.Peek().minVal;
+    }
+}
